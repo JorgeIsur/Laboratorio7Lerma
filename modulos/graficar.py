5975361893eeea207221624aba3b030caa777f47
JorgeIsur <ramirezisur@gmail.com>
2022-11-20 19:59:09 -0600

    removi los labels del eje x

--- modulos/graficar.py
+++ modulos/graficar.py
@@ -4,6 +4,6 @@ def ploteo(x,y):
     plt.grid(True,color="gray")
     plt.xlabel("tiempo")
     plt.ylabel("y")
-    plt.yticks([-1,-0.5,0,0.5,1])
+    #plt.yticks([-1,-0.5,0,0.5,1])
     plt.plot(x,y)
     plt.show()
